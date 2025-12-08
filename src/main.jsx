26141087710422e91bd000bcddb47851a013ddbc
SeppeGhekiere <seppe.ghekiere@student.ehb.be>
2025-12-08 09:40:35 +0100

    added react-router to add scene to different urls

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,55 @@
 // main.jsx
 import { createRoot } from "react-dom/client";
-import SceneManager from "./SceneManager"; // Corrected path
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+import Project from "./scenes/Project.jsx";
+import MachineCorridor from "./scenes/MachineRoom";
+import Circles from "./scenes/Circles.jsx";
+import Angel from "./scenes/Angel.jsx";
+import FullAngelScene from "./scenes/FullAngelScene.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<SceneManager />);
+function Root() {
+	return (
+		<div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", zIndex: 1 }}>
+			<Canvas shadows camera={{ position: [0, 0, 0] }} style={{ width: "100%", height: "100%", display: "block" }}>
+				<Suspense fallback={null}>
+					<Outlet />
+				</Suspense>
+			</Canvas>
+		</div>
+	);
+}
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		children: [
+			{
+				index: true,
+				element: <Project />,
+			},
+			{
+				path: "machine-corridor",
+				element: <MachineCorridor />,
+			},
+			{
+				path: "circles",
+				element: <Circles />,
+			},
+			{
+				path: "angel-model",
+				element: <Angel />,
+			},
+			{
+				path: "full-scene-angel",
+				element: <FullAngelScene />,
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
